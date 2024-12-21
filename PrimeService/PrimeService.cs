7e234d76e92610deb63ab2e394ef80aed91c9e9e
breno085 <br3noacioli@gmail.com>
2024-12-21 19:39:34 -0300

    datarow atributes to test difent inputs

---
+++ PrimeService/PrimeService.cs
@@ -0,0 +1,14 @@
+﻿namespace PrimeServices;
+
+public class PrimeService
+{
+    public bool IsPrime(int candidate)
+    {
+        if (candidate < 2)
+        {
+            return false;
+        }
+
+        throw new NotImplementedException("Please create a test first.");
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Documentation
+
+[Unit testing C# with MSTest and .NET](https://learn.microsoft.com/en-us/dotnet/core/testing/unit-testing-with-mstest)  
+
+# Tutorial
